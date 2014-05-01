de96cdde254b9b3e8ca7597154aef50bb39c5144
David <david@jaguttis.ca>
2014-04-30 20:19:32 -0500

    First go

---
+++ site/index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js"><!--<![endif]-->
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+	<title>Factor</title>
+	<link href='http://fonts.googleapis.com/css?family=Concert+One' rel='stylesheet' type='text/css'>
+	
+	<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	
+	<!-- <link rel="stylesheet" href="css/style.min.css" type="text/css" media="screen"> -->
+	<!--[if IE]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+<body>
+
+	<div class="menu">
+		<div class="container clearfix">
+
+			<div class="nav grid_6 alpha omega">
+
+				<ul class="navigation">
+					<li data-slide="2">Music</li>
+					<li data-slide="3">Bio/Pics</li>
+					<li data-slide="5">Side Road<span class="decorative"> Records</span></li>
+				</ul>
+			</div>
+			
+			<div class="nav grid_6 alpha omega yestablet">
+			
+				<ul class="social">
+					<li><a href="http://facebook.com/factorchandelier"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="http://instagram.com/indefinitearticle"><i class="fa fa-instagram"></i></a></li>
+					<li><a href="http://twitter.com/factormusic"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="https://youtube.com"><i class="fa fa-youtube"></i></a></li>
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
+
+
+	<div class="slide" id="slide1" data-slide="1" data-stellar-background-ratio="0.5">
+		<div class="container clearfix">
+
+			<div class="grid_12 center">
+				<h1 id="fittext2">FACTOR</h1>
+			</div>
+
+		</div>
+	</div>
+	
+	<div class="slide" id="slide2" data-slide="2" data-stellar-background-ratio="0.5">
+		<div class="container clearfix">
+
+			<div class="grid_6">
+				<br>
+				<iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/6202966&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
+			</div>
+			<div class="grid_1 omega alpha">
+			</div>	
+			<div class="grid_5">
+				<div>
+					<h2><i class="fa fa-music">&nbsp;</i>Music</h2>
+					<a class="button" href="https://soundcloud.com/factorchandelier">SoundCloud</a><br>
+					<a class="button" href="http://factorchandelier.bandcamp.com">Bandcamp</a><br>
+					<a class="button" href="https://itunes.apple.com/us/artist/factor/id151285155?uo=4" target="itunes_store" <a href="https://itunes.apple.com/us/artist/factor/id151285155?uo=4" target="itunes_store">iTunes</a>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+	
+		
+	<div class="slide" id="slide3" data-slide="3" data-stellar-background-ratio="0.5">
+		<div class="container clearfix">
+
+			<div class="grid_8">
+				<h2><i class="fa fa-microphone"></i> Bio</h2>
+
+				<p>Saskatoon-based super-producer Factor (née Graham Murawsky) broke into the music scene as a DJ in 1998, drawing early production inspiration from the West Coast underground and collaborations with emcees such as Kirby Dominant, along with fellow Canadian emcees Kay the Aquanaut &amp; Def3 en route to forming Side Road Records. His innovative beats are grounded in a lush, organic hip hop structure with alternating rock, folk, pop and soul sensibilities. As the years and his sound progressed, Factor’s work caught the attention of Fake Four Inc., culminating in a diverse array of projects both solo and collaborative.</p>
+
+				<p>Since joining the Connecticut based indie label in 2008, Factor has helmed full-lengths for the likes of Myka 9, Awol One, Moka Only and Gregory Pepper while provided production for alt-rap icons such as Sole and Ceschi. Though he has diagrammed his share of filthy breaks and meticulous synths the old-fashioned way, Factor has also called in session musicians and crafted sample-free records, most notably 2011’s melodic &amp; soulful, <em>Factor and the Chandeliers</em>.</p>
+
+				<p>The guest lists on Factor’s solo albums (e.g.<em> Lawson Graham</em>, <em>13 Stories</em>) have showcased a wide spectrum of modern underground talent, with this trend continuing on 2013’s <em>Woke Up Alone</em>, a brilliant necromancy concept record framed by the five stages of grief which features a number of the aforementioned Fake Four artists in addition to Astronautalis, Onry Ozzborn and Open Mike Eagle.</p>
+
+				<p>Factor’s travels across the continents have seen him share stages and tracks with top tier indie hip-hop artists like Aesop Rock and Buck 65 to Xzibit and Kool Keith. There are maybe a handful of independent hip hop producers whose distinct styles are recognizable within 10 seconds of the beat dropping, and the prolific, in-demand Factor has joined the pantheon of Dilla, Madlib, El-P and Blockhead in sharing such honors.</p>
+				
+				<p><strong>Email</strong></br>
+				&#102;&#97;&#99;&#116;&#111;&#114;&#105;&#115;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</p>
+			</div>
+			<div class="grid_1">
+			</div>	
+			<div class="grid_3 omega">
+				<h2><i class="fa fa-bolt"></i> Pics</h2>
+				<p><a href="images/Factor-by_Kaid_Ashton.jpg" target="_blank"><img src="images/Factor-by_Kaid_Ashton-sm.jpg" alt="Factor by Kaid Ashton" width="100%" /></a></p>
+				<p><a href="images/Factor-by_Kaid_Ashton2.jpg" target="_blank"><img src="images/Factor-by_Kaid_Ashton2-sm.jpg" alt="Factor by Kaid Ashton" width="100%" /></a></p>
+				<p><a href="images/Factor-by_Chad_Coombs.jpg" target="_blank"><img src="images/Factor-by_Chad_Coombs-sm.jpg" alt="Factor by Chad Coombs" width="100%" /></a></p>
+				<p><a href="images/Factor-by_Ghostshrimp.jpg" target="_blank"><img class="nopad" src="images/Factor-by_Ghostshrimp-sm.jpg" alt="Factor by Ghostshrimp" width="100%" /></a></p>
+				
+			</div>
+
+		</div>
+	</div>
+	
+	
+	<div class="slide nopad" id="slide4" data-slide="4" data-stellar-background-ratio="0.5">
+		<div id="instafeed"></div>
+	</div>
+	
+	
+	<div class="slide logos" id="slide5" data-slide="5" data-stellar-background-ratio="0.5">
+		<div class="container clearfix">
+			<div class="grid_12">
+				<a href="http://sideroadrecords.com"><img src="images/sideroad-logo.png" alt="Side Road Records" /></a>
+				<a href="http://fakefourinc.com"><img src="images/fakefour-logo.png" alt="Side Road Records" /></a>
+				<a href="http://artsboard.sk.ca/"><img src="images/SAB_Logo_Reverse.png" alt="Saskatchewan Arts Board" /></a>
+				<a href="http://creativesask.ca"><img src="images/CreativeSask_logo.png" alt="Creative Saskatchewan" /></a>
+				<a href="http://saskmusic.org"><img src="images/saskmusic_logo.png" alt="SaskMusic" /></a>			
+			</div>
+		</div>
+	</div>
+	
+	
+
+	<div class="credits center">
+		&copy;<?php echo date('Y'); ?> Graham Murawsky <a href="http://319heads.com">Design by 319 Heads</a>
+	</div>
+
+
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script type="text/javascript" src="js/waypoints.min.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/instafeed.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	<script type="text/javascript" src="js/jquery.fittext.js"></script>
+	
+	<script type="text/javascript">
+		var feed = new Instafeed({
+		get: 'user',
+		limit: 3,
+		resolution: 'standard_resolution', 
+		userId: 9236478,
+		accessToken: '386430072.467ede5.94c8a9969ff34cb08da78d2274a98fcc'
+		});
+	    feed.run();
+	</script>
+
+
+	
+	
+	<script type="text/javascript">
+		$("#fittext2").fitText(0.33);
+	</script>
+
+</body>
+</html>
